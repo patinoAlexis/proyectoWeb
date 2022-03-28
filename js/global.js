ce238288b587a3c1ab9cbf855f6d1c8e985fcdbc
patinoAlexis <patino.alexis@uabc.edu.mx>
2022-03-28 00:16:15 -0700

    Version 0.7 - Resposinve design
    Los cambios en esta versión se hicieron todos en JS,  se organizo todo lo que era el archivo de global.css, y se hizo de que el nav del header te dirigiera a cada segmento. También una gran parte del desarrollo fue para el responsive design, donde si se cambia de tamaño la pagina web, esta debería de poder mantener una buena forma, aunque cabe destacar de que el resposive design no esta hecho de la mejor manera, muy probablemente no vuelva a haber cambios para mejorar esta área, ya que acumula mucho tiempo. si llegaran a haber cambios serian después de la versión 1.0

--- js/global.js
+++ js/global.js
@@ -1,4 +1,54 @@
-$(document).ready(
-
+$(document).ready(function (){
     
-)
+    $('.op-nav').click(function(){
+        
+        let scrollTo;
+        switch($(this).text()){
+            case 'Videojuegos':
+                scrollTo = '#videojuegos';
+                break;
+            case 'Futbol':
+                scrollTo = '#futbol';
+                break;
+            case 'Esports':
+                scrollTo = '#esports';
+                break;
+            case 'Programacion':
+                scrollTo = '#programacion';
+                break;
+        }
+        $([document.documentElement, document.body]).animate({
+            scrollTop: $(scrollTo).offset().top
+        }, 10);/*
+        let position = scrollTo.offset().top;
+        console.log($('html, body').scrollTop());
+        $('html, body').animate({scrollTop: position}, 0);*/
+       
+    })
+    $(window).resize(function size_html(){
+        let size = $('.main-header').width();
+        if(size <= 1800){
+            if( size <= 1200){
+                $('img').css("height", 220);
+                $('.sub-seg').css('width','100%')
+                $('h3').css('font-size','1.2rem')
+            } else{
+                $('img').css("height", (300/1800) * size);
+            }
+            
+        } else{
+            $('img').css("height",300)
+        }
+    })
+    if($(window).width() <= 1800){
+        if( $(window).width() <= 1200){
+            $('img').css("height", 220);
+            $('.sub-seg').css('width','100%')
+            $('h3').css('font-size','1.2rem')
+        } else{
+            $('img').css("height", (300/1800) * size);
+        }
+    }else{
+        $('img').css("height",300)
+    }
+});
