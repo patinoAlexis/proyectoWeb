9d6c244c963373f459eea162755f1dd650ab0a48
Alexis Patiño <patino.alexis@uabc.edu.mx>
2022-04-18 18:27:43 -0700

    Version 0.8 - Sección videojuegos terminada
    En este commit se hicieron varios cambios al responsive design, para que a la hora de calibrar la pagina web, las imágenes y textos pudieran verse de buena manera. También el producto final de este commit fue terminar la sección de videojuegos, llenándola de información y con 2 carruseles funcionales

--- js/dist/global.dev.js
+++ js/dist/global.dev.js
@@ -1,6 +1,38 @@
 "use strict";
 
 $(document).ready(function () {
+  $('.owl-carousel').owlCarousel({
+    loop: true,
+    center: true,
+    autoWidth: true,
+    margin: 2,
+    nav: true,
+    responsive: {
+      0: {
+        items: 1
+      },
+      600: {
+        items: 1
+      },
+      1000: {
+        items: 1
+      }
+    }
+  });
+
+  function elim_agr_columna() {
+    var col = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : '';
+    $('sub-seg').removeClass(function (index, css) {
+      return (css.match(/\bcol-\S+/g) || []).join(' ');
+    });
+
+    if (col == 'col-12') {
+      $('.sub-seg').addClass(col);
+    } else {
+      $('.sub-seg').addClass();
+    }
+  }
+
   $('.op-nav').click(function () {
     var scrollTo;
 
@@ -37,24 +69,86 @@ $(document).ready(function () {
       if (size <= 1200) {
         $('img').css("height", 220);
         $('.sub-seg').css('width', '100%');
-        $('h3').css('font-size', '1.2rem');
+        $('.nav-link h3').css('font-size', '1.1rem');
       } else {
+        $('.sub-seg').each(function () {
+          if ($(this).hasClass('col-3')) {
+            $(this).css('width', '25%');
+          } else if ($(this).hasClass('col-4')) {
+            $(this).css('width', '30%');
+          } else if ($(this).hasClass('col-5')) {
+            $(this).css('width', '41%');
+          } else if ($(this).hasClass('col-6')) {
+            $(this).css('width', '50%');
+          } else if ($(this).hasClass('col-7')) {
+            $(this).css('width', '58%');
+          }
+        });
         $('img').css("height", 300 / 1800 * size);
       }
     } else {
+      $('.sub-seg').each(function () {
+        if ($(this).hasClass('col-3')) {
+          $(this).css('width', '25%');
+        } else if ($(this).hasClass('col-4')) {
+          $(this).css('width', '30%');
+        } else if ($(this).hasClass('col-5')) {
+          $(this).css('width', '41%');
+        } else if ($(this).hasClass('col-6')) {
+          $(this).css('width', '50%');
+        } else if ($(this).hasClass('col-7')) {
+          $(this).css('width', '58%');
+        }
+      });
+      $('.nav-link h3').css('font-size', '1.7rem');
       $('img').css("height", 300);
     }
+
+    $(".owl-carousel").each(function (index, el) {
+      var containerHeight = $(el).height();
+      var containerWidth = $(el).width();
+      $(el).find("img").each(function (index, img) {
+        var w = $(img).prop('naturalWidth');
+        var h = $(img).prop('naturalHeight');
+        $(img).css({
+          'width': Math.round(w * (containerWidth / w)) + 'px',
+          'height': Math.round(h * (containerWidth / w)) + 'px'
+        });
+      }), $(el).owlCarousel({
+        autoWidth: true
+      });
+    });
   });
 
   if ($(window).width() <= 1800) {
+    var size = $('.main-header').width();
+
     if ($(window).width() <= 1200) {
       $('img').css("height", 220);
       $('.sub-seg').css('width', '100%');
-      $('h3').css('font-size', '1.2rem');
+      $('.nav-link h3').css('font-size', '1.1rem');
     } else {
+      $('.nav-link h3').css('font-size', '1.7rem');
       $('img').css("height", 300 / 1800 * size);
     }
   } else {
+    $('.nav-link h3').css('font-size', '1.7rem');
     $('img').css("height", 300);
   }
+});
+jQuery(document).ready(function ($) {
+  $(".owl-carousel").each(function (index, el) {
+    var containerHeight = $(el).height();
+    var containerWidth = $(el).width();
+    $(el).find("img").each(function (index, img) {
+      var w = $(img).prop('naturalWidth');
+      var h = $(img).prop('naturalHeight');
+      $(img).css({
+        'width': Math.round(w * (containerWidth / w)) + 'px',
+        'height': Math.round(h * (containerWidth / w)) + 'px'
+      });
+    }), $(el).owlCarousel({
+      autoWidth: true
+    });
+  });
 });
--- js/global.js
+++ js/global.js
@@ -1,5 +1,33 @@
 $(document).ready(function (){
-    
+    $('.owl-carousel').owlCarousel({
+        loop:true,
+        center: true,
+        autoWidth:true,
+        margin:2,
+        nav:true,
+        responsive:{
+            0:{
+                items:1
+            },
+            600:{
+                items:1
+            },
+            1000:{
+                items:1
+            }
+        }
+    })
+    function elim_agr_columna(col = ''){
+        $('sub-seg').removeClass(function (index, css) {
+            return (css.match (/\bcol-\S+/g) || []).join(' ');
+        });
+        if(col == 'col-12'){
+            $('.sub-seg').addClass(col)
+        } else {
+            $('.sub-seg').addClass()
+        }
+        
+    }
     $('.op-nav').click(function(){
         
         let scrollTo;
@@ -30,25 +58,89 @@ $(document).ready(function (){
         if(size <= 1800){
             if( size <= 1200){
                 $('img').css("height", 220);
-                $('.sub-seg').css('width','100%')
-                $('h3').css('font-size','1.2rem')
+                $('.sub-seg').css('width','100%');
+                $('.nav-link h3').css('font-size','1.1rem')
             } else{
+                $('.sub-seg').each(function(){
+                    if($(this).hasClass('col-3')){
+                        $(this).css('width','25%');
+                    }else if($(this).hasClass('col-4')){
+                        $(this).css('width','30%');
+                    }else if($(this).hasClass('col-5')){
+                        $(this).css('width','41%');
+                    }else if($(this).hasClass('col-6')){
+                        $(this).css('width','50%');
+                    }else if($(this).hasClass('col-7')){
+                        $(this).css('width','58%')
+                    }
+                })
                 $('img').css("height", (300/1800) * size);
             }
             
         } else{
-            $('img').css("height",300)
+            $('.sub-seg').each(function(){
+                if($(this).hasClass('col-3')){
+                    $(this).css('width','25%');
+                }else if($(this).hasClass('col-4')){
+                    $(this).css('width','30%');
+                }else if($(this).hasClass('col-5')){
+                    $(this).css('width','41%');
+                }else if($(this).hasClass('col-6')){
+                    $(this).css('width','50%');
+                }else if($(this).hasClass('col-7')){
+                    $(this).css('width','58%')
+                }
+            })
+            $('.nav-link h3').css('font-size','1.7rem')
+            $('img').css("height",300);
         }
+        $(".owl-carousel").each(function(index, el) {
+            var containerHeight = $(el).height();
+            var containerWidth = $(el).width();
+            $(el).find("img").each(function(index, img) {
+              var w = $(img).prop('naturalWidth');
+              var h = $(img).prop('naturalHeight');
+              $(img).css({
+                'width': Math.round(w * (containerWidth / w)) + 'px',
+                'height': Math.round(h * (containerWidth / w)) + 'px',
+              });
+            }),
+            $(el).owlCarousel({
+              autoWidth: true
+            });
+        });
     })
     if($(window).width() <= 1800){
+        let size = $('.main-header').width();
         if( $(window).width() <= 1200){
             $('img').css("height", 220);
             $('.sub-seg').css('width','100%')
-            $('h3').css('font-size','1.2rem')
+            $('.nav-link h3').css('font-size','1.1rem')
+            
         } else{
+            $('.nav-link h3').css('font-size','1.7rem')
             $('img').css("height", (300/1800) * size);
         }
     }else{
+        $('.nav-link h3').css('font-size','1.7rem')
         $('img').css("height",300)
     }
+    
 });
+jQuery(document).ready(function($) {
+    $(".owl-carousel").each(function(index, el) {
+      var containerHeight = $(el).height();
+      var containerWidth = $(el).width();
+      $(el).find("img").each(function(index, img) {
+        var w = $(img).prop('naturalWidth');
+        var h = $(img).prop('naturalHeight');
+        $(img).css({
+          'width': Math.round(w * (containerWidth / w)) + 'px',
+          'height': Math.round(h * (containerWidth / w)) + 'px',
+        });
+      }),
+      $(el).owlCarousel({
+        autoWidth: true
+      });
+    });
+  });
